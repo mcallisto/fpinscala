0d6c195374b215104e2b9d426f949a6a939b3db3
mcallisto <mario.callisto@gmail.com>
2015-03-18 22:57:00 +0100

    Exercise 10 completed

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -125,6 +125,16 @@ object RNG {
       val (a, rng2) = f(rng)
       g(a)(rng2)
     }
+  
+  def positiveIntF: Rand[Int] =
+    flatMap(int)(i ⇒ if (i != Int.MinValue) unit(i.abs) else positiveIntF)
+    
+  // EXERCISE 10: Reimplement map and map2 in terms of flatMap
+  def mapF[A, B](s: Rand[A])(f: A ⇒ B): Rand[B] =
+    flatMap(s)(a ⇒ unit(f(a)))
+
+  def map2F[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) ⇒ C): Rand[C] =
+    flatMap(ra)(a ⇒ flatMap(rb)(b ⇒ unit(f(a, b))))
 }
 
 case class State[S, +A](run: S ⇒ (A, S)) {
