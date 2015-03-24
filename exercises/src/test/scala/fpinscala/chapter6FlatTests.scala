81479cbc5a5fff2196233f7c621007a1e912aa47
mcallisto <mario.callisto@gmail.com>
2015-03-24 12:57:37 +0100

    More tests

--- exercises/src/test/scala/fpinscala/chapter6FlatTests.scala
+++ exercises/src/test/scala/fpinscala/chapter6FlatTests.scala
@@ -30,4 +30,35 @@ class chapter6FlatTests extends FlatSpec {
     assert(i === 10)
   }
 
+  it can "map a result" in {
+    val (i, r) = RNG.map(RNG.unit(10))(_ + 1)(seed)
+    assert(i === 11)
+  }
+
+  it can "generate a random positive Int not greater than a given value" in {
+    val (i, r) = RNG.positiveMax(1000)(seed)
+    assert(i.isValidInt && i >= 0 && i <= 1000)
+  }
+
+  it can "generate a random Double " in {
+    val (i, r) = RNG.double(seed)
+    val (i2, r2) = RNG.doubleM(r)
+    assert(i !== i2)
+  }
+
+  it can "generate a random positive Int" in {
+    val (i, r) = RNG.positiveInt(seed)
+    assert(i.isValidInt && i >= 0)
+  }
+
+  it can "generate a list of random Int" in {
+    val (i, r) = RNG.ints(5)(seed)
+    assert(i.forall(_.isValidInt) && i.size == 5)
+  }
+
+  it can "map2 two result" in {
+    val (i, r) = RNG.map2(RNG.unit(10), RNG.unit(11))(_ + _)(seed)
+    assert(i === 21)
+  }
+
 }
